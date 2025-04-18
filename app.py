ccdb91ae0e99cdecfab9acad22ab4134b17b890c
Arpan Patra <arpanpatra800188500@gmail.com>
2025-04-18 13:02:48 +0530

    Update app2

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/view')
+def view():
+    return render_template('index2.html')
+@app.route('/contact')
+def contact():
+    return render_template('Contact.html')
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=port)
